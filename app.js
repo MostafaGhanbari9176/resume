54fa61ad4ed503cd1fc3c0e2b3e4d4b001ec2de1
Mostafa <godhelot1@outlook.com>
2020-10-16 14:13:09 +0330

    implement main page

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
+const app = express()
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('./index', {pageTitle: "MostafaGhanbari"})
+})
+
+app.listen(8083)
