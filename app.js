2b54c0c07bad1763600c39d51430b93d1b80962c
Mostafa <godhelot1@outlook.com>
2020-10-28 11:02:00 +0330

    work on projects section

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use(express.static(path.join(__dirname, '/node_modules//@fortawesome/fontawesome-free')))
+app.use(express.static(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const images = {
+    doreyab:[
+        {img:"/img/doreyab/ic.png", selected:true},
+        {img:"/img/doreyab/ic2.png", selected:false}
+    ],
+    mr:[
+        {img:"/img/mr/ic.png", selected:true},
+        {img:"/img/mr/ic2.png", selected:false}
+    ]
+}
+
+function setFirstImage(){
+
+}
