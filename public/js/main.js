454f9228fc3fa53e74a6ba637f218c4ed8fa2671
Mostafa <godhelot1@outlook.com>
2020-10-29 10:11:13 +0330

    add resources

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,95 @@ const imageList = [
     {
         name: "mr",
         images: [
-            {img: "/img/doreyab/ic.png", showing: false},
-            {img: "/img/doreyab/ic2.png", showing: false}
+            {img: "/img/mr/ic.png", showing: false},
+            {img: "/img/mr/1.jpg", showing: false},
+            {img: "/img/mr/2.jpg", showing: false},
+            {img: "/img/mr/3.jpg", showing: false},
+            {img: "/img/mr/4.jpg", showing: false},
+            {img: "/img/mr/5.jpg", showing: false}
+        ]
+    },
+    {
+        name: "reminder",
+        images: [
+            {img: "/img/reminder/ic.png", showing: false},
+            {img: "/img/reminder/1.jpg", showing: false},
+            {img: "/img/reminder/2.jpg", showing: false},
+            {img: "/img/reminder/3.jpg", showing: false},
+            {img: "/img/reminder/4.jpg", showing: false},
+            {img: "/img/reminder/5.jpg", showing: false},
+            {img: "/img/reminder/6.jpg", showing: false}
+        ]
+    },
+    {
+        name: "les",
+        images: [
+            {img: "/img/les/1.png", showing: false},
+            {img: "/img/les/2.png", showing: false},
+            {img: "/img/les/3.png", showing: false},
+            {img: "/img/les/4.png", showing: false},
+            {img: "/img/les/5.png", showing: false},
+            {img: "/img/les/6.png", showing: false},
+            {img: "/img/les/7.png", showing: false},
+            {img: "/img/les/8.png", showing: false},
+            {img: "/img/les/9.png", showing: false},
+            {img: "/img/les/10.png", showing: false},
+            {img: "/img/les/11.png", showing: false},
+            {img: "/img/les/13.png", showing: false},
+            {img: "/img/les/14.png", showing: false},
+            {img: "/img/les/15.png", showing: false},
+            {img: "/img/les/16.png", showing: false},
+            {img: "/img/les/17.png", showing: false},
+        ]
+    },
+    {
+        name: "coursefinder",
+        images: [
+            {img: "/img/coursefinder/ic.png", showing: false},
+            {img: "/img/coursefinder/1.png", showing: false},
+            {img: "/img/coursefinder/2.png", showing: false},
+            {img: "/img/coursefinder/3.png", showing: false},
+            {img: "/img/coursefinder/4.png", showing: false},
+            {img: "/img/coursefinder/5.png", showing: false},
+            {img: "/img/coursefinder/6.png", showing: false},
+            {img: "/img/coursefinder/7.png", showing: false},
+            {img: "/img/coursefinder/8.png", showing: false},
+            {img: "/img/coursefinder/9.png", showing: false},
+            {img: "/img/coursefinder/10.png", showing: false},
+            {img: "/img/coursefinder/11.png", showing: false},
+            {img: "/img/coursefinder/12.png", showing: false},
+            {img: "/img/coursefinder/13.png", showing: false},
+            {img: "/img/coursefinder/14.png", showing: false},
+            {img: "/img/coursefinder/15.png", showing: false},
+            {img: "/img/coursefinder/16.png", showing: false},
+            {img: "/img/coursefinder/17.png", showing: false},
+            {img: "/img/coursefinder/18.png", showing: false},
+            {img: "/img/coursefinder/19.png", showing: false},
+            {img: "/img/coursefinder/20.png", showing: false},
+            {img: "/img/coursefinder/21.png", showing: false},
+            {img: "/img/coursefinder/22.png", showing: false},
+            {img: "/img/coursefinder/23.png", showing: false},
+            {img: "/img/coursefinder/24.png", showing: false},
+        ]
+    },
+    {
+        name: "awesome",
+        images: [
+            {img: "/img/awesome/1.png", showing: false},
+            {img: "/img/awesome/2.png", showing: false},
+            {img: "/img/awesome/3.png", showing: false},
+            {img: "/img/awesome/4.png", showing: false},
+            {img: "/img/awesome/5.png", showing: false},
+            {img: "/img/awesome/12.png", showing: false},
+            {img: "/img/awesome/6.png", showing: false},
+            {img: "/img/awesome/7.png", showing: false},
+            {img: "/img/awesome/8.png", showing: false},
+            {img: "/img/awesome/9.png", showing: false},
+            {img: "/img/awesome/10.png", showing: false},
+            {img: "/img/awesome/11.png", showing: false},
+            {img: "/img/awesome/13.png", showing: false},
+            {img: "/img/awesome/14.png", showing: false},
+            {img: "/img/awesome/15.png", showing: false}
         ]
     }
 ]
@@ -55,3 +142,9 @@ const resetOpenedImages = () => {
         value.showing = false
     })
 }
+
+$('a').attr('target', '_blank')
+
+$('a[href=""]').click(ev => {
+    ev.preventDefault()
+}).css('color', '#999').attr('title', 'هنوز راه اندازی نشده😊')
